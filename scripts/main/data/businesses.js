3c80cbf93bdfc46c82fca1d53e2b15f96fc2f36a
Ibigoni Inye-Tam <inyetamibigoni@gmail.com>
2025-07-26 21:59:34 -0500

    Creatd business js to store data for each businesses

---
+++ scripts/main/data/businesses.js
@@ -0,0 +1,156 @@
+export const businesses = [
+  {
+    id: "2a50bc06-45e1-4b1b-ba8f-73ccf0721125",
+    name: "Computer Greeks",
+    category: "Tech",
+    description: {
+      brief: "Your favorite tech in town. Get your devices fixed for a low price.",
+      detail: ""
+    },
+    hours: {
+      monday: "10:00 AM - 6:00 PM",
+      tuesday: "10:00 AM - 6:00 PM",
+      wednesday: "10:00 AM - 6:00 PM",
+      thursday: "10:00 AM - 6:00 PM",
+      friday: "10:00 AM - 6:00 PM",
+      saturday: "10:00 AM - 6:00 PM",
+      sunday: "closed"
+    },
+    contact: {
+      phone : "708-636-3473",
+      email: "info@computergreeks.com",
+      address: "12222 S Harlem Ave, Palos Heights, IL"
+    },
+    image: {
+      logo: "images/business-listings/computer-greeks.png",
+      moreImages : [
+        "images/business-detail-images/computer-greeks/computer-greeks1.webp",
+        "images/business-detail-images/computer-greeks/computer-greeks2.webp",
+        "images/business-detail-images/computer-greeks/computer-greeks3.webp"
+      ],
+      rating: {
+        one: 4.7,
+        two: 4.1,
+        three: 4.5
+      }
+    },
+    rating: 4.5,
+    comments: {
+      "Tara Ross": {
+        rating: "⭐⭐⭐⭐",
+        text: "Toby is excellent. So knowledgeable and passionate about his work. Highly recommend! Shop local!"
+      },
+      "Jacob Brown": {
+        rating: "⭐⭐⭐⭐⭐",
+        text: "The people here are very nice. They even fixed up my pc in two businesses days. Very fast and great customer service. Thanks for fixing my pc. Highly recommend going here for any PC issues."
+      },
+      "Basil Toures": {
+        rating: "⭐⭐⭐⭐⭐",
+        text: "They were able to extract the data out of my broken desktop and I also purchased a new mini desktop from them . Great personal service. I highly recommend them."
+      }  
+    }   
+  },
+  {
+    id: "a52d1662-5787-4973-bcc9-5f0a45206cb0",
+    name: "K&G Fashion Superstore",
+    category: "Clothing",
+    description: {
+      brief: "Best, affordable clothing in your local area.",
+      detail: ""
+    },
+    hours: {
+      monday: "10:00 AM - 9:00 PM",
+      tuesday: "10:00 AM - 9:00 PM",
+      wednesday: "10:00 AM - 9:00 PM",
+      thursday: "10:00 AM - 9:00 PM",
+      friday: "10:00 AM - 9:00 PM",
+      saturday: "10:00 AM - 9:00 PM",
+      sunday: "11:00 AM - 7:00 PM"
+    },
+    contact: {
+      phone : "708-418-5707",
+      email: "customerservice@kgstores.com",
+      address: "17149 Torrence Ave, Lansing, IL"
+    },
+    image: {
+      logo: "images/business-listings/k-g-logo-png-transparent.png",
+      moreImages : [
+        "images/business-detail-images/k&G-fashion/k-g-store1.webp",
+        "images/business-detail-images/k&G-fashion/k-g-store3.webp",
+        "images/business-detail-images/k&G-fashion/k-g-store4.webp"
+      ],
+      rating: {
+        one: 4.6,
+        two: 4.0,
+        three: 4.3
+      }
+    },
+    rating: 4.0,
+    comments: {
+      "Juanita Buie": {
+        rating: "⭐⭐⭐⭐⭐",
+        text: "I absolutely love K&G for our family casual & dressy fashion needs! I just purchased these shoes for my son! Very Nice!"
+      },
+      "Ro mell": {
+        rating: "⭐⭐⭐⭐",
+        text: "This place has a really good selection of men's suits. Make sure you try on before you buy. Suits are made to look and feel good on you."
+      },
+      "Edward Lewis": {
+        rating: "⭐⭐⭐⭐⭐",
+        text: "K&G has all the fashion for men. No matter your size. I love their suits, hats, and very stylish shoes."
+      }  
+    }   
+  },
+  {
+    id: "62c0ca28-98bf-4ea3-8e58-74327a63b198",
+    name: "Just Be Coffee Roasters",
+    category: "Food",
+    description: {
+      brief : "Best coffee in town. Locally roasted and ethically sourced.",
+      detail : "Just Be Coffee Roasters is a specialty coffee roastery based in Munster, Indiana, committed to community, sustainability, and high-quality coffee. They source 100% Arabica, specialty grade beans from around the world including Brazil, Kenya, Colombia, and Honduras and roast them locally. Their offerings include unique single-origin coffees and signature blends, available for retail and wholesale partners. With a focus on ethical sourcing and a zero waste refill and reuse program, Just Be Coffee Roasters aims to make a positive environmental and community impact one cup at a time."
+    },  
+    hours: {
+      monday: "7:00 AM - 3:00 PM",
+      tuesday: "7:00 AM - 3:00 PM",
+      wednesday: "7:00 AM - 3:00 PM",
+      thursday: "7:00 AM - 3:00 PM",
+      friday: "7:00 AM - 3:00 PM",
+      saturday: "8:00 AM - 12:00 PM",
+      sunday: "8:00 AM - 12:00 PM"
+    },
+    contact: {
+      phone : "219-789-8354",
+      email: "sales@justbe.coffee",
+      address: "400 Fisher St unit e, Munster, IN"
+    },
+    image: {
+      logo: "images/business-listings/just-be-coffe-roasters.png",
+      moreImages : [
+        "images/business-detail-images/just-be-coffee/just-be-coffe.jpg",
+        "images/business-detail-images/just-be-coffee/just-be-coffee-2.jpg",
+        "images/business-detail-images/just-be-coffee/just-be-coffee-3.webp"
+      ],
+      rating: {
+        one: 4.3,
+        two: 4.2,
+        three: 4.4
+      }
+    },
+    rating: 4.9,
+    comments: {
+      "Carina Crisan": {
+        rating: "⭐⭐⭐⭐⭐",
+        text: "This has become my favorite coffee shop in the area. I have no good reason to choose a coffee selling chain on Calumet over this. The service is relatively quick, the baristas/owners are always very nice, and the coffee is great quality fosr a great price."
+      },
+      "Jeffrey Blueblaze Yoga": {
+        rating: "⭐⭐⭐",
+        text: "If you are riding the Penske bike trail this is a very good place to stop for coffee. The atmosphere is funky and friendly. They roast their own coffee here."
+      },
+      "Adam White": {
+        rating: "⭐⭐⭐⭐⭐",
+        text: "I love having a local coffee shop to visit. The staff members are patient, courteous, and attentive.They have a well stocked storefront and a good area of chairs and seating."
+      }  
+    }    
+  }
+];
+
